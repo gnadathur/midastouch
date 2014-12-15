f0e99f28160a5b73f6e03193f97d842f71862968
Gokul Nadathur <gokul.nadathur@gmail.com>
2014-12-14 21:51:02 -0800

    convert to unix filetype

--- securify.py
+++ securify.py
@@ -1,5 +1,5 @@
 # -------------------------------------------------------------------------------
-# Name:        module1
+# Name:       PasswordProtect 
 # Purpose:
 #
 # Author:      gokul
@@ -9,6 +9,7 @@
 # Licence:     <your licence>
 # -------------------------------------------------------------------------------
 import keyring
+import optparse
 
 
 class PasswordProtect(object):
@@ -18,3 +19,21 @@ class PasswordProtect(object):
     def getPass(self, username):
         pswd = keyring.get_password(self.service, username)
         return pswd
+
+    def setPass(self, username, pswd):
+        keyring.set_password(self.service, username, pswd)
+
+
+def main():
+    p = optparse.OptionParser()
+    p.add_option("-b", "--baseurl", type="string", action="store", dest="url")
+    p.add_option("-u", "--userid", type="string", action="store", dest="userid")
+    p.add_option("-p", "--password", type="string", action="store", dest="pswd")
+    opt, _ = p.parse_args()
+
+    p = PasswordProtect(opt.url)
+    p.setPass(opt.user, opt.pswd)
+
+
+if __name__ == '__main__':
+    main()
